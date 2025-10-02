069913063037d04ca4cb1f540a316e074318953d
Nitheesh Keerthi Veettil Ramakrishnan <nitheesh.r@tryg.no>
2025-10-02 16:48:39 +0200

    integrted the post list with backend api

--- react-refresher-course/src/components/PostList/PostList.jsx
+++ react-refresher-course/src/components/PostList/PostList.jsx
@@ -2,15 +2,31 @@ import NewPost from "../NewPost/NewPost";
 import Modal from "../Modal/Modal";
 import Post from "../Post/Post";
 import classes from "./PostList.module.css";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 function PostList({ isModalVisible, setisModalVisible }) {
   const [postList, setPostList] = useState([]);
 
   const handleNewPostCreation = (body, author) => {
+    fetch("http://localhost:8080/posts", {
+      method: 'POST',
+      body: JSON.stringify({ body, author }),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
     setPostList((existingPosts) => [{ body, author }, ...existingPosts]);
     setisModalVisible(false);
   };
+
+  useEffect(() => {
+    async function fetchPosts() {
+      const response = await fetch("http://localhost:8080/posts");
+      const data = await response.json();
+      setPostList(data.posts);
+    }
+    fetchPosts();
+  }, []);
 
   let modalContent = isModalVisible && (
     <Modal closeModal={() => setisModalVisible(false)}>
